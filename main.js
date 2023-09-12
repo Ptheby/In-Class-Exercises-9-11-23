3649d18a6fd2aabcd1fd5d714d47a2f6908cee0f
Paul Theby <paultheby@gmail.com>
2023-09-12 14:33:12 -0500

    9-11-23 In class exercises

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 //Exercise #1
-///1.1
-function functionOne() {
-  console.log(this);
-}
+// ///1.1
+// function functionOne() {
+//   console.log(this);
+// }
 
-const firstObject = {
-  name: "Ball",
-  saying: function () {
-    console.log(this);
-  },
-};
-const secondObject = {
-  name: "Tire",
-  printName: () => {
-    console.log(this);
-  },
-};
-functionOne();
-firstObject.saying();
-secondObject.printName();
+// const firstObject = {
+//   name: "Ball",
+//   saying: function () {
+//     console.log(this);
+//   },
+// };
+// const secondObject = {
+//   name: "Tire",
+//   printName: () => {
+//     console.log(this);
+//   },
+// };
+// functionOne();
+// firstObject.saying();
+// secondObject.printName();
 
 //1.2 ^^^
 ///1.3 First was global object, second is the object and in this case
@@ -27,7 +27,7 @@ secondObject.printName();
 
 ///Exercise 2  2.1-2.3
 
-//2.1-2.2
+2.1-2.2
 class Person {
   constructor(name, age) {
     this.name = name
@@ -36,9 +36,22 @@ class Person {
   }
    greeting() {
     console.log(`Hello ${this.name} !`);
+   
   };
+  static info() {
+    return "This is a person class"
+};
+
 }
 let person1 = new Person("Mckenna", 12);
 let person2 = new Person("Walker", 5);
 person1.greeting();
 person2.greeting();
+Person.info();
+
+
+
+// //Exercise 3
+// already did it this way
+
+// Exercise 5
